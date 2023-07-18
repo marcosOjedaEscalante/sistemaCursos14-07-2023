f73ade5e90420f1246851c8ea71c2849c82391ee
Marcos Ojeda Escalante <marcos.ojeda.escalante@gmail.com>
2023-07-17 20:52:48 -0400

    conexión actualizada

--- src/cnn/cnn.js
+++ src/cnn/cnn.js
@@ -1,3 +1,3 @@
 import { Sequelize } from "sequelize";
 
-export const sequelize = new Sequelize('postgres://postgres:root@localhost:5432/casoEstudiantes');
+export const sequelize = new Sequelize('postgres://postgres:qbvieSQcYMtxpvL569fr@containers-us-west-136.railway.app:7729/railway');
