b9d4f27a2c1fc4d6d14470ab22fbab3d718d3ac2
Marcos Ojeda Escalante <marcos.ojeda.escalante@gmail.com>
2023-08-01 21:13:01 -0400

    validacion usuario, creación JWT

--- src/controllers/auth.controller.js
+++ src/controllers/auth.controller.js
@@ -1,11 +1,33 @@
 import { request, response } from "express";
+import bcryptjs from 'bcryptjs';
+import { Usuario } from "../models/usuarios.js";
+import { generarJWT } from "../helpers/jwt.js";
 
-const loginUsuario = (req = request, res = response) => {
+const loginUsuario = async (req = request, res = response) => {
+    const { correo, password } = req.body;
+    // Validar el correo existencia
+    const usuario = await Usuario.findOne({ where: { correo } });
+    if(!usuario){
+        return res.json({
+            msg: 'El correo o contraseña son inválidos - correo'
+        });
+    }
+    // Validar la contraseña
+    const passwordValido = bcryptjs.compareSync(password, usuario.password);
+    if(!passwordValido){
+        return res.json({
+            msg: 'El correo o contraseña son inválidos - contraseña'
+        });
+    }
+    // construir el token
+    const token = await generarJWT(usuario.id);
+
     res.json({
-        msg: 'login ok'
+        msg: 'Login funcionando',
+        token
     });
 }
 
-export{
+export {
     loginUsuario
 }
